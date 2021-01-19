48eb2cd5b29539ec2ce547d2c4f2373eaef8865e
doodream <macbook@Macbookui-MacBookPro.local>
2021-01-20 01:14:01 +0900

    Verify : bcrypt로 비밀번호를 변경해서 개발자도 모르게 사용자의 비밀번호 변경

--- models/User.js
+++ models/User.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+// salt를 이용해서 비밀번호를 암호화하는데 saltRounds는 salt의 글자 수이다.
+const saltRounds = 10;
+
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +35,28 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number,
     }
+})
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    // user 스키마중에 password가 입력이 될때만 암호화 진행 
+    if (user.isModified('password')) {
+        //사용자 비밀번호를 받아서 암호화 후 전송
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            // err 발생시 user.save() 에 err 전달
+            if (err) return next(err);
+            // salt가 잘 생성되었다면 
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                // err가 뜨면 err message를 user.save()에 전달
+                if (err) return next(err);
+                // 비밀번호를 암호화해서 교체
+                user.password = hash;
+                next();
+            });
+        });
+    }
+
 })
 
 
